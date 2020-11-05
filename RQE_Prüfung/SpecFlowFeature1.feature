62b6be783da8516d510600da2c724ff4b36596fb
Michael Zeiner <mzmichael05@gmail.com>
2020-11-05 10:42:11 +0100

    Add project files.

---
+++ RQE_Prüfung/SpecFlowFeature1.feature
@@ -0,0 +1,23 @@
+﻿Feature: Taschenrechner
+    In order to avoid silly mistakes
+    As a math idiot
+    I want to be told the sinus, cosinus and tangens of a number
+
+@mytag
+Scenario: Calc Sinus
+    Given the number is 3,1415
+    When i want the Sinus
+    Then the result should be 0
+
+
+Scenario: Calc Cosinus
+    Given the number 3.1415
+    When i want the Cosinus
+    Then the result should be -1
+
+
+Scenario Outline: Calc Tangens
+    Given the number 3.1415
+    When I want the Tangens
+    Then the result should be 0
+
---
+++ RQE_Prüfung/TaschenrechnerSteps.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.CommonModels;
+using Xunit;
+
+namespace RQE_Prüfung
+{
+    [Binding]
+    public class TaschenrechnerSteps
+    {
+
+        var result = 0;
+
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the number (.*)")]
+        public void GivenTheNumber(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"i want the Sinus")]
+        public void WhenIWantTheSinus()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"i want the Cosinus")]
+        public void WhenIWantTheCosinus()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I want the Tangens")]
+        public void WhenIWantTheTangens()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = result;
+            var actual = p0;
+            Assert.Equal(expected, actual, 5);
+        }
+    }
+}
